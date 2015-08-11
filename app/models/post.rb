1a22ad862a82ed5886263f71c92b85e7c53e599d
telwell <me@trevorelwell.com>
2015-08-10 20:35:47 -0400

    adding likeable concerns

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,20 +6,10 @@ class Post < ActiveRecord::Base
 	# CONCERN: This feels brittle... does a post really 
 	# have one profile? Perhaps. 
 	has_one :profile, :through => :user
-	has_many :likes, :as => :likeable, :dependent => :destroy
 	has_many :comments, :as => :commentable, :dependent => :destroy
 
 	# End associations
 
-	# Return true if a user has liked a particular post
-	def liked_by?(user)
-		self.likes.pluck(:user_id).include?(user.id) ? true : false
-	end
-
-	# Return the like_id given the post and the user
-	def like_id(user)
-		like = self.likes.where("user_id = ? ", user.id)
-		like.first.id
-	end
+	include Likeable
 
 end
